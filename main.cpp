9760240d02c35eb3231933f29affebfc33376099
Armin <72748909+ahadzic7@users.noreply.github.com>
2022-10-11 16:12:26 +0200

    radi tarjan

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@ void print_graph(const std::vector<std::list<int>> &graph) {
     }
 }
 
+void print_scc(std::vector<std::list<int>> scc) {
+    for(auto & list : scc) {
+        for(auto el: list)
+            printf("%d ", el);
+        printf("\n");
+    }
+}
+
 void readData(int &crossings, int &streets, std::vector<std::list<int>> &graph) {
     scanf("%d %d", &crossings, &streets);
 
@@ -28,64 +36,66 @@ void readData(int &crossings, int &streets, std::vector<std::list<int>> &graph) 
 //    print_graph(graph);
 }
 
-//-----------------------------------------------------------GLOBAL VARIABLES---------------------------------
+void findSCC(int crossing, std::vector<int> &lowlinks, std::stack<int> &components, std::vector<std::list<int>> &graph, std::vector<bool> &inStack, std::vector<int> &index, std::vector<std::list<int>> &sccLists,int counter = 0) {
+    lowlinks[crossing] = counter;
+    index[crossing] = counter;
+    counter++;
 
-std::vector<std::list<int>> sccLists;
-//-----------------------------------------------------------GLOBAL VARIABLES---------------------------------
-
-
-void findSCC(int crossing, std::vector<int> &lowlinks, std::stack<int> &components, std::vector<std::list<int>> &graph, std::vector<bool> &inStack) {
-    lowlinks[crossing] = crossing;
     components.push(crossing);
     inStack[crossing] = true;
 
     for(const auto &node: graph[crossing]) {
-        if(lowlinks[node] == -1) {
-            findSCC(node, lowlinks, components, graph, inStack);
+        if(index[node] == 0) {
+            findSCC(node, lowlinks, components, graph, inStack, index, sccLists,counter);
             if(lowlinks[node] < lowlinks[crossing])
                 lowlinks[crossing] = lowlinks[node];
         }
 
         else if(inStack[node]) {
-            if(node < lowlinks[crossing])
-                lowlinks[crossing] = node;
+            if(index[node] < lowlinks[crossing])
+                lowlinks[crossing] = index[node];
         }
     }
 
-    if(lowlinks[crossing] == crossing) {
+    if(lowlinks[crossing] == index[crossing]) {
         std::list<int> component;
         int node;
+
         do {
             node = components.top();
             components.pop();
             component.push_back(node);
             inStack[node] = false;
         } while (node != crossing);
+
         sccLists.push_back(component);
     }
 
 }
 
+void filterWeekCrossings(std::vector<std::list<int>> sccLists) {
+
+}
 
 void solution() {
     int crossings, streets;
     std::vector<std::list<int>> graph(0);
 
     readData(crossings, streets, graph);
+
     std::stack<int> components;
     std::vector<bool> inStack(crossings, false);
     std::vector<int> lowlinks(crossings, -1);
+    std::vector<int> index(crossings, 0);
+    std::vector<std::list<int>> sccLists;
 
 
     for(int i = 0; i < crossings; i++) {
-        findSCC(i, lowlinks, components, graph, inStack);
+        if(index[i] == 0)
+        findSCC(i, lowlinks, components, graph, inStack, index, sccLists);
     }
 
-    for(auto & sccList : sccLists) {
-        for(auto el: sccList)
-            printf("%d ", el);
-        printf("\n");
-    }
+//    print_scc(sccLists);
 
 
 
